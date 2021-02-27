2a39acb44f0d3604c4e3ced040c9517e1566aff9
h2 <ht@live.se>
2021-02-27 23:59:44 +0800

    update

---
+++ src/main/java/com/respok/admin/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.respok.admin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String hello(@RequestParam("name") String name){
+        return "hello," +name + "!";
+    }
+}
